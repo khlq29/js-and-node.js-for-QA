8bd4b930586dc9ab2624a2cabc421bb6d2ca8001
khlq29 <asimasu31@gmail.com>
2024-03-03 13:01:38 +0530

    arrow function

---
+++ wk4/functions/arrowFn.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * Arrow functions
+ * 
+ * @scenario
+ * 1. convert a normal function to arrow function
+ * 2. no paaram
+ * 3. single param
+ * 4. more than one param (single and more than one statements)
+ * 5. forEach() method example
+ * 
+ */
+
+
+// 1. convert a normal function to arrow function
+
+function addTwoNum(num1, num2){
+    let sum = num1 + num2
+     return sum
+  }
+  
+
+
